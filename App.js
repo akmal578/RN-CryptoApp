c797b946e7dd03742751ceef7e3172cccdd474a6
Akmal Adnan <68601221+akmal578@users.noreply.github.com>
2021-03-15 09:46:19 +0800

    initial

--- App.js
+++ App.js
@@ -1,36 +1,34 @@
 import React from 'react';
-import { CryptoDetail, Transaction } from "./screens";
-import { createStackNavigator } from "@react-navigation/stack";
+import { CryptoDetail, Transaction } from './screens';
+import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+//Status bar: 'default', 'dark-content', 'light-content'
 
-import Tabs from "./navigation/tabs";
+import Tabs from './navigation/tabs';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle={'light-content'}
+      />
       <Stack.Navigator
         screenOptions={{
-          headerShown: false
+          headerShown: false,
         }}
         initialRouteName={'Home'}
       >
-        <Stack.Screen
-          name="Home"
-          component={Tabs}
-        />
-        <Stack.Screen
-          name="CryptoDetail"
-          component={CryptoDetail}
-        />
-        <Stack.Screen
-          name="Transaction"
-          component={Transaction}
-        />
+        <Stack.Screen name="Home" component={Tabs} />
+        <Stack.Screen name="CryptoDetail" component={CryptoDetail} />
+        <Stack.Screen name="Transaction" component={Transaction} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
 export default App;
